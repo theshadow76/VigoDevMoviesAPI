5d73965bd3847eb79bec96b535ecb704dc3f9cc4
Vigo Walker <vigopaul05@gmail.com>
2023-03-02 20:18:02 -0300

    updated to v0.0.2

--- src/main/java/dev/vigowalke/movies/Movie.java
+++ src/main/java/dev/vigowalke/movies/Movie.java
@@ -1,12 +1,24 @@
 package dev.vigowalke.movies;
 
+import dev.vigowalke.movies.Review;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Document(collation = "movies")
+@RestController
+@Document(collection = "movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Movie {
+    @Id
     private ObjectId id;
     private String imdbId;
     private String title;
@@ -15,4 +27,6 @@ public class Movie {
     private String poster;
     private List<String> genres;
     private List<String> backdrops;
+    @DocumentReference
+    private  List<Review> reviewsIds;
 }
